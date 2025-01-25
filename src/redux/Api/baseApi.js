f9513169743831ace96cb332e38191f07829d1a0
unknown <foisalrk2@gmail.com>
2025-01-25 12:54:11 +0600

    s

--- src/redux/Api/baseApi.js
+++ src/redux/Api/baseApi.js
@@ -2,7 +2,7 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://143.110.241.146:9050";
+const baseUrl = "http://143.110.241.146:9050/api";
 
 // Helper function to get the token
 const getToken = () => {
